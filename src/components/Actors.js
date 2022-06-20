2bfa3012b3559525c76592f28e9f347ecb546385
Breon White <bdwhite92@gmail.com>
2022-06-20 12:58:12 -0400

    commit tree cd1169f6f080bae234870dcde07ccd52d1f8a05a

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsList = actors.map((actor) => {
+    return (
+      <div key={actor.name}>
+        <h3>{actor.name}</h3>
+        <ul>
+          {actor.movies.map((actorMovie) => (
+            <li key={actorMovie}>{actorMovie}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  })
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsList}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorsList = directors.map((director) => {
+    return (
+      <div key={director.name}>
+        <h3>{director.name}</h3>
+        <ul>
+          {director.movies.map((directorMovie) => (
+            <li key={directorMovie}>{directorMovie}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  })
+  
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorsList}
+    </div>
+  );
 }
 
 export default Directors;
